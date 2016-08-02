7474b5cf704e6ac56f9c5085a912f5fd6dde9c94
Ryan Gribble <ryan.gribble@tattsgroup.com>
2016-08-02 14:23:12 +1000

    trying to get movie detail panel working

---
+++ src/app/list/movie-panel.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input, Output, EventEmitter} from "@angular/core";
+import {ROUTER_DIRECTIVES} from "@angular/router";
+import {Movie} from "../models/movie";
+
+@Component({
+    selector: "movie-panel",
+    templateUrl: "./movie-panel.component",
+    directives: [ROUTER_DIRECTIVES]
+})
+export class MoviePanelComponent {
+    @Input() movie: Movie;
+    @Output() navigate = new EventEmitter<number>();
+
+    constructor() {
+    }
+
+    goToMovie() {
+        this.navigate.emit(this.movie.id);
+    }
+
+    rateMovie(movie: Movie) {
+        const result = {
+            good: false,
+            bad: false
+        };
+
+        if (movie.rating > 4) {
+            result.good = true;
+        } else if (movie.rating < 2) {
+            result.bad = true;
+        }
+
+        return result;
+    }
+}
